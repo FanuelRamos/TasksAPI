1f4baa02108a11dfadfbc9ab7e14de250108b86b
Fanuel Ramos <fanuelramos111@gmail.com>
2022-10-29 14:19:58 +0100

    Added The Database Connection
    Added the Database Connection and because of it now the server runs only if it success to connect with database.

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,11 @@
+import mongoose from 'mongoose';
 import { app } from  './app';
 
-app.listen(3000);
+console.log('Running');
+
+try {
+  mongoose.connect('mongodb://127.0.0.1:27017/Tasks');
+  app.listen(3000);
+} catch (error) {
+  console.log(error);
+}
