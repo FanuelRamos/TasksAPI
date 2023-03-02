202f116757385dbf2c4621b11cef08d87d1d9023
Fanuel Ramos <fanuelramos111@gmail.com>
2023-03-03 00:45:34 +0100

    feat: ensure CreateTaskUseCase returns a task if secceeds

--- src/useCases/CreateTask/CreateTaskUseCase.ts
+++ src/useCases/CreateTask/CreateTaskUseCase.ts
@@ -5,9 +5,14 @@ import { ICreateTaskRequestDTO } from "./CreateTaskDTO";
 export class CreateTaskUseCase {
   constructor (private taskRepository: ITaskRepository) {}
 
-  async execute(data: ICreateTaskRequestDTO) {
+  async execute(data: ICreateTaskRequestDTO): Promise<Task> {
     const task = new Task(data);
 
+    // Should validate if the start date is not a past date
+    // Should validate if finish date is not less then start date
+
     await this.taskRepository.create(task);
+
+    return task
   }
 }
