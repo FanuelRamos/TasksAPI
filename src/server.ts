05a1d77cb34ff4083b103b705bd0a3f7a8267e88
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-10 00:20:49 +0100

    feat: add mongodb cluster uri

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from  './app';
 
 try {
-  mongoose.connect('mongodb://127.0.0.1:27017/Task');
+  mongoose.connect('mongodb+srv://fanuelramos:11PTszA7bPrdmFKL@taskapi.zeapays.mongodb.net/?retryWrites=true&w=majority');
   app.listen(3000);
   console.log('Running');
 } catch (error) {
