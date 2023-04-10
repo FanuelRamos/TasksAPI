5271839dd41ac22fbf5aca6bb0e5f2bdbbe0c809
Fanuel Ramos <fanuelramos111@gmail.com>
2023-04-10 10:55:09 +0100

    chore: add dotenv on the app

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { app } from  './app';
+dotenv.config()
 
 try {
-  mongoose.connect('mongodb+srv://fanuelramos:11PTszA7bPrdmFKL@taskapi.zeapays.mongodb.net/?retryWrites=true&w=majority');
-  app.listen(3000);
+  mongoose.connect(process.env.MONGO_URI);
+  app.listen(process.env.PORT);
   console.log('Running');
 } catch (error) {
   console.log(error);
